4ffbd9c0e4099c8e819f70f4b2a0aed56b7293b8
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-09-20 15:45:34 -0400

    updated file

--- myProject/github_portfolio_v2/src/App.js
+++ myProject/github_portfolio_v2/src/App.js
@@ -1,12 +1,15 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Profile from './pages/Profile';
+
 
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo} />
+      <Profile userName="mojo-KB" />
     </div>
   );
 }
--- myProject/github_portfolio_v2/src/pages/Profile.js
+++ myProject/github_portfolio_v2/src/pages/Profile.js
@@ -1,5 +1,43 @@
 import { useState, useEffect } from 'react';
 
 function Profile( {userName} ) {
+   const [loading, setloading] = useState(false);
+   const [profile, setProfile] = useState({});
+
+   useEffect( () => {
+        async function fetchData() {
+            const profile = await fetch(
+                `https://api.github.com/users/${userName}`
+            );
+            const result = await profile.json();
+
+
+            if(result) {
+                setProfile(result);
+                setloading(false);
+            }
+        }
+    fetchData();
     
+   }, [userName]);
+
+   return (
+    <div>
+        <h2>About me</h2>
+        {
+            loading ? (
+                <span>Loading...</span>
+            ) : ( <ul>
+                <li>
+                <span>Avatar_url: </span>
+                
+                {profile.avatar_url}
+                </li>
+                
+                </ul>)
+        }
+    </div>
+   );
 }
+
+export default Profile;
