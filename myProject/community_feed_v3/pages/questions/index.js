820e0c75dda0b989813334ad777aaee46af7deea
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-09-28 03:23:13 -0400

    fetching data file

--- myProject/community_feed_v3/pages/questions/index.js
+++ myProject/community_feed_v3/pages/questions/index.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { useState, useEffect } from 'react'
+import Card from '../../components/Card'
 
 
 const QuestionContainer = styled.div`
@@ -10,9 +12,42 @@ const QuestionContainer = styled.div`
 
 
 function Questions() {
-    return(
+    const [loading, setLoading] = useState(false)
+    const [questions, setQuestions] = useState([])
+    
+    useEffect( () => {
+        async function fetchData() {
+            const data = await fetch('https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&tagged=reactjs&site=stackoverflow');
+
+            const result = await data.json();
+
+            if(result) {
+                setQuestions(result.items)
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, []);
+
+
+
+    return (
         <QuestionContainer>
-            <h2>Questions</h2>
+            <h2>Questions</h2> 
+            {
+                loading ?(<span>Loading...</span>) : (
+                    <div>
+                        {questions.map( (question)=> (
+                            <Card 
+                                key={question.question_id}
+                                title={question.title}
+                                views={question.view_count}
+                                answers={question.answer_count}
+                            />
+                        ))}
+                    </div>
+                )
+            }
         </QuestionContainer>
     )
 }
