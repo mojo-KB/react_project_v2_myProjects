552894b2b06aeecac7dd88582ead3fece6bb99b0
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-09-27 22:35:11 -0400

    upload community feeds

--- myProject/todolist/src/App.js
+++ myProject/todolist/src/App.js
@@ -2,19 +2,31 @@
 import Header from './components/Header/Header'
 import Board from './pages/Board/Board';
 import Backlog from './pages/Backlog/Backlog'
-import styled from 'styled-components'
+import styled, { createGlobalStyle} from 'styled-components'
 
-
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+`
 const AppWrapper = styled.div`
   text-align: center;
 `
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <AppWrapper> 
       <Header />
       <Board />
       <Backlog /> 
     </AppWrapper>
+    </>
   );
 }
 
